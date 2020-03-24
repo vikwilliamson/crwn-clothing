f7dd75f4adf0a87ffaf43455d3bcfe848a2831a3
Vik Williamson <b009058@a05064080.federated.fds>
2020-03-24 13:42:16 -0400

    added sign-in-sign-up page & components+styles

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInSignUpPage} />
       </Switch>
     </div>
   );
